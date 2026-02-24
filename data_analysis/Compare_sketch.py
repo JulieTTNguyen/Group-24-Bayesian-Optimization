b772c0ce531c214d6e8cacaa9d7ce05615f6abbe
KatinkaMG <s235058@dtu.dk>
2026-02-24 14:51:22 +0100

    added comments

--- data_analysis/Compare_sketch.py
+++ data_analysis/Compare_sketch.py
@@ -6,9 +6,9 @@ def softmax(x):
 for trial in data:
     for i,point in enumerate(trial[4:]):
         x,y = point
-        mu, std, ? = GP(trial[:i+1])
-        aq_vals = expected_improvement(current_best, mu, std)
-
+        mu, std, ? = GP(trial[:i+1]) #opdater med den rigtige Gaussian process funktion
+        aq_vals = expected_improvement(current_best, mu, std) #kør med den relavante aquisition funktion
+        beta = 5 # prøv forskellige værdier
         sm = softmax(beta * aq_vals)
         sm[x][y] # nok ikke sådan det er indexed
         #gem denne sftmax et sted
