56cb916195cf57c49ddcb1ee4b0b2142666743cd
avkol22 <s234136@dtu.dk>
2026-02-24 14:04:58 +0100

    coordinates done

--- data_loading/load_coordinates.py
+++ data_loading/load_coordinates.py
@@ -1,9 +1,10 @@
-from data_loading.load_human_data import xyz
+from load_human_data import xyz
 import numpy as np
 
-print(np.shape(xyz))  # Check the shape of the loaded data
+
 coordinates = []
 
 
 for pair in range(len(xyz)):    
     coordinates.append((xyz[pair,:, :2]))  # Extract x and y coordinates
+print(coordinates)
